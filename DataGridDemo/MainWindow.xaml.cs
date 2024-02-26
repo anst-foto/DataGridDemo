af67721da2e9e7764acf0dd1547b775672bee204
Starinin Andrey <starininandrey@gmail.com>
2024-02-26 21:04:29 +0300

    Update

--- DataGridDemo/MainWindow.xaml.cs
+++ DataGridDemo/MainWindow.xaml.cs
@@ -1,23 +1,78 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using DataGridDemo.Models;
+using MahApps.Metro.Controls;
 
 namespace DataGridDemo;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : MetroWindow, INotifyPropertyChanged
 {
+    public ObservableCollection<Person> Persons { get; set; }
+    
     public MainWindow()
     {
+        Persons = new ObservableCollection<Person>()
+        {
+            new Person()
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "<EMAIL>",
+                IsDeleted = false,
+                DateOfBirth = new DateTime(1985, 1, 1),
+                Gender = Gender.Male
+            },
+            new Person()
+            {
+                Id = 2,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "<EMAIL>",
+                IsDeleted = true,
+                DateOfBirth = new DateTime(1985, 1, 1),
+                Gender = Gender.Male
+            },
+            new Person()
+            {
+                Id = 3,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "<EMAIL>",
+                IsDeleted = false,
+                DateOfBirth = new DateTime(1985, 1, 1),
+                Gender = Gender.Female
+            },
+            new Person()
+            {
+                Id = 4,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "mailto:mail@mail.ru",
+                IsDeleted = false,
+                DateOfBirth = new DateTime(1985, 1, 1),
+                Gender = Gender.Male
+            },
+        };
+        
         InitializeComponent();
     }
+    
+    #region INotifyPropertyChanged
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+    #endregion
 }
---
+++ DataGridDemo/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace DataGridDemo.Models;
+
+public enum Gender
+{
+    Male,
+    Female
+}
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public Gender Gender { get; set; }
+}
